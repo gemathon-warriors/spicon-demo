4e865a700c16e7a8d999340427dc2036934a81de
Nikhil Nanjappa <nikhil@pls-bug-me-not.(none)>
2014-02-22 01:41:16 +0530

    * Gemathon-warriors logo * Few more interesting changes ;-) Adding gem names on hover

---
+++ app/assets/javascripts/skywalker.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $('.timezonify').tooltip(
+    	{trigger: "hover",
+    	 title: "Timezonify gem",
+    	  placement: "left",
+    	   html: "true"})
+
+    $('.tareek').tooltip(
+	{trigger: "hover",
+	 title: "Tareek gem",
+	  placement: "right",
+	   html: "true"})
+
+    $('.contineo').tooltip(
+	{trigger: "hover",
+	 title: "Contineo gem",
+	  placement: "left",
+	   html: "true"})
+
+    $('.bugs').tooltip(
+	{trigger: "hover",
+	 title: "Report bugs or Issues",
+	  placement: "right",
+	   html: "true"})
+
+     $('.central_img').tooltip(
+	{trigger: "hover",
+	 title: "Nothing Interesting here !!",
+	  placement: "right",
+	   html: "true"})
+});
